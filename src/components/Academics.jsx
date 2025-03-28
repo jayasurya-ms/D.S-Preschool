a61fccef071e25f3401d2dba36cfd1e6f97fb24b
unknown <supersurya.ms@gmail.com>
2025-03-28 20:20:02 +0530

    Modrated modification

--- src/components/Academics.jsx
+++ src/components/Academics.jsx
@@ -3,14 +3,13 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Academics.css";
-import { color } from "framer-motion";
 
 function Academics() {
   const academics = [
-    { id: 1, image: "src/assets/breeds/Labrador.jpg", name: "Toddler", description: "Discovery and exploration, language development, new-age skills, and shaping scientific mindset." },
-    { id: 2, image: "src/assets/breeds/doberman.jpg", name: "Play group", description: "Enhancing interaction, cultivating creativity and improving imagination through art and music." },
-    { id: 3, image: "src/assets/breeds/shih-tzu3.webp", name: "Nursery", description: "Age-appropriate learning and educational tools suited for individual pace." },
-    { id: 4, image: "src/assets/breeds/German-shepherd.jpg", name: "LKG", description: "Developing vocabulary, attention span, reading and writing skills with innovative methods and preparing for primary school." }
+    { id: 1, image: "src/assets/play_group.jpg", name: "Play group", description: "Discovery and exploration, language development, new-age skills, and shaping scientific mindset." },
+    { id: 2, image: "src/assets/breeds/doberman.jpg", name: "Nursery", description: "Enhancing interaction, cultivating creativity and improving imagination through art and music." },
+    { id: 3, image: "src/assets/breeds/shih-tzu3.webp", name: "LKG", description: "Age-appropriate learning and educational tools suited for individual pace." },
+    { id: 4, image: "src/assets/breeds/German-shepherd.jpg", name: "UKG", description: "Developing vocabulary, attention span, reading and writing skills with innovative methods and preparing for primary school." }
   ];
 
   const settings = {
@@ -46,32 +45,38 @@ function Academics() {
 
   return (
     <section
-      id="popular-breeds"
+      id="Academics"
       style={{ textAlign: "center", padding: "40px", backgroundColor: "#f3e5f5" }}
     >
-      <h1 >Academics</h1>
-      <Slider {...settings} className="div-a">
-        {academics.map((academic) => (
-          <div key={academic.id} className="div-ai">
-            <div className="card">
-              <img
-                src={academic.image}
-                alt={academic.name}
-                style={imageStyle}
-              />
-              <h3 style={titleStyle}>{academic.name}</h3>
-              <p style={descriptionStyle}>{academic.description}</p>
+      <div className="spacer w-100"></div>
+      <div className="p-0 m-0 w-100 h-100">
+        <h1 >Academics</h1>
+        <Slider {...settings} className="div-a">
+          {academics.map((academic) => (
+            <div key={academic.id} className="div-ai">
+              <div className="card">
+                <img
+                  src={academic.image}
+                  alt={academic.name}
+                  style={imageStyle}
+                />
+                <h3 style={titleStyle}>{academic.name}</h3>
+                <p style={descriptionStyle}>{academic.description}</p>
+              </div>
             </div>
-          </div>
-        ))}
-      </Slider>
+          ))}
+        </Slider>
+      </div>
+      <div className="h-100 w-100 p-0 m-0 mt-5 bg-dark">
+          hii
+      </div>
     </section>
   );
 }
 
 const imageStyle = {
   width: "100%",
-  height: "150px", // Fixed height for images
+  height: "200px", // Fixed height for images
   // objectFit: "contain",
   borderRadius: "10px",
   backgroundPosition: 'center',
