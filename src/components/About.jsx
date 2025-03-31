4b4708371e1989742964b875acd645f8ac323c40
unknown <supersurya.ms@gmail.com>
2025-03-31 13:36:36 +0530

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,32 @@
 import React from "react";
+import "../style/About.css";
 
 function About() {
   return (
     <section id="About" className="contactsection" style={{ backgroundColor: "#f3e5f5" }}>
       <div className="spacer w-100"></div>
-      <div style={{ display: "flex", padding: "50px", justifyContent: "space-between", alignItems: 'center' }}>
-        <img src="src\Assets\aboutus.png" alt="Why Choose Us" style={{ borderRadius: "50%", width: "600px" }} className="image shadow " />
-        <div className="p-4 " style={{ marginLeft: '2%', marginTop: '3%' }}>
-          <h1 className="p-2 " style={{ backgroundColor: "pink", width: "320px", borderRadius: "6px" }}>Why Choose Us? </h1>
-          <h1>D.S Educational Trust Preschool and Daycare is one of the finest schools in BTM layout.</h1>
-          
-            <p className="fs-5" >School management takes extra efforts for the care and safety of kids and staffs. </p>
-            <p className="fs-5" >Children are taught practically things and they have excercise activities as a everyday schedule, 
-                to keep good fit. On the order of priority, caretakers, staffs and management put lot of potential efforts to get kid good foundation. </p>
-          
-          <button style={buttonStyle}> <a className="form-link" href="https://docs.google.com/forms/d/e/1FAIpQLScf6actMecVBa2hly2ztjmOHBaH9VzQ0Ee4c2-11ayaCgCoog/viewform?usp=sharing" >
-            ContactUs
-          </a></button>
+      <div className="p-4">
+        <div className="w-100 d-flex justify-content-center p-3">
+          <img src="src\Assets\aboutus.png" alt="Why Choose Us" className="Title" />
+        </div>
+        <div className="row m-0">
+          <div className="col-md-4 p-3 d-flex justify-content-center align-items-center">
+            <img src="" alt="about img" className="rounded-circle " />
+          </div>
+          <div className="col-md-8 p-3 shadow-lg rounded-3">
+            <h1 className="p-2 about-head">Why Choose Us? </h1>
+            <h2 className="about-heading">D.S Educational Trust Preschool and Daycare is one of the finest schools in BTM layout.</h2>
+
+            <p>School management takes extra efforts for the care and safety of kids and staffs. </p>
+            <p>Children are taught practically things and they have excercise activities as a everyday schedule,
+              to keep good fit. On the order of priority, caretakers, staffs and management put lot of potential efforts to get kid good foundation. </p>
+            <a
+              href="https://docs.google.com/forms/d/e/1FAIpQLScf6actMecVBa2hly2ztjmOHBaH9VzQ0Ee4c2-11ayaCgCoog/viewform?usp=sharing"
+              className="btn btn-lg px-4 shadow-sm"
+            >
+              Reach us
+            </a>
+          </div>
         </div>
       </div>
     </section>
